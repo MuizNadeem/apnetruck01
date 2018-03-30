2c86c253e9d9ab93ba22975ec0f9675a35966507
MuizNadeem <muiz.nadeem@gmail.com>
2018-03-30 09:33:37 +0530

    db connection

--- app.js
+++ app.js
@@ -4,14 +4,26 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database');
+
+//MongoDB
+mongoose.connect(config.database);
+//Good Connection
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database ' + config.database)
+});
+//Connection error
+mongoose.connection.on('error', (err) => {
+    console.log('Database Error ' + err)
+});
+
 
 const app = express();
-
-const users = require('./routes/users');
 
 //Port number
 const port = 3000;
 
+const users = require('./routes/users');
 
 //     Middleware
 //To serve requests coming outside 1 domain
@@ -26,11 +38,11 @@ app.use(bodyParser.json());
 //Routes
 app.use('/users', users);
 
-/*temporary Index route 
+/*temporary Index route */
 app.get('/', (req, res) => {
     res.send('Invalid Endpoint');
 });
-*/
+
 //Start Server and Listen on specific port
 app.listen(port, () => {
     console.log('Server started on port ' + port);
